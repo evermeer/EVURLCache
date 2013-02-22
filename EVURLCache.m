b8f160152edae8d6ae07318fe04ddd82a95a9ffe
Edwin Vermeer <edwin@evict.nl>
2013-02-22 08:17:34 +0100

    Improvement to the addSkipBackupAttributeToItemAtURL method

--- EVURLCache.m
+++ EVURLCache.m
@@ -170,9 +170,25 @@ static NSString* _preCacheDirectory;
 + (BOOL)addSkipBackupAttributeToItemAtURL:(NSURL *)URL {
     const char* filePath = [[URL path] fileSystemRepresentation];
     const char* attrName = "com.apple.MobileBackup";
-    u_int8_t attrValue = 1;
-    int result = setxattr(filePath, attrName, &attrValue, sizeof(attrValue), 0, 0);
-    return result == 0;
+    if (&NSURLIsExcludedFromBackupKey == nil) {
+        // iOS 5.0.1 and lower
+        u_int8_t attrValue = 1;
+        int result = setxattr(filePath, attrName, &attrValue, sizeof(attrValue), 0, 0);
+        return result == 0;
+    } else {
+        // First try and remove the extended attribute if it is present
+        int result = getxattr(filePath, attrName, NULL, sizeof(u_int8_t), 0, 0);
+        if (result != -1) {
+            // The attribute exists, we need to remove it
+            int removeResult = removexattr(filePath, attrName, 0);
+            if (removeResult == 0) {
+                NSLog(@"Removed extended attribute on file %@", URL);
+            }
+        }
+        
+        // Set the new key
+        return [URL setResourceValue:[NSNumber numberWithBool:YES] forKey:NSURLIsExcludedFromBackupKey error:nil];
+    }
 }
 
 +(BOOL)networkAvailable{
