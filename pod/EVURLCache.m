12770b6a5f81875f8d7a521e507362def710299a
Edwin Vermeer <edwin@evict.nl>
2018-07-16 07:37:57 +0200

    check for selector

--- pod/EVURLCache.m
+++ pod/EVURLCache.m
@@ -116,7 +116,7 @@ static NSString* _preCacheDirectory;
 {
     NSFileManager *fileManager = [NSFileManager defaultManager];
     NSHTTPURLResponse *httpResponse = cachedResponse.response;
-    if(httpResponse != nil && httpResponse.statusCode >= 400)
+    if(httpResponse != nil && [httpResponse respondsToSelector:@selector(statusCode:)] && httpResponse.statusCode >= 400)
     {
         NSLog(@"CACHE Do not cache error %li page for: %@", (long)httpResponse.statusCode, request.URL);
         return;
@@ -235,26 +235,18 @@ static NSString* _preCacheDirectory;
 +(BOOL)addSkipBackupAttributeToItemAtURL:(NSURL*)URL{
     const char* filePath = [[URL path] fileSystemRepresentation];
     const char* attrName = "com.apple.MobileBackup";
-    if (&NSURLIsExcludedFromBackupKey == nil) {
-        // iOS 5.0.1 and lower
-        u_int8_t attrValue = 1;
-        int result = setxattr(filePath, attrName, &attrValue, sizeof(attrValue), 0, 0);
-        return result == 0;
-    } else {
-        // First try and remove the extended attribute if it is present
-        int result = getxattr(filePath, attrName, NULL, sizeof(u_int8_t), 0, 0);
-        if (result != -1) {
-            // The attribute exists, we need to remove it
-            int removeResult = removexattr(filePath, attrName, 0);
-            if (removeResult == 0) {
-                NSLog(@"Removed extended attribute on file %@", URL);
-            }
+    // First try and remove the extended attribute if it is present
+    int result = getxattr(filePath, attrName, NULL, sizeof(u_int8_t), 0, 0);
+    if (result != -1) {
+        // The attribute exists, we need to remove it
+        int removeResult = removexattr(filePath, attrName, 0);
+        if (removeResult == 0) {
+            NSLog(@"Removed extended attribute on file %@", URL);
         }
-        
-        // Set the new key
-        return [URL setResourceValue:[NSNumber numberWithBool:YES] forKey:NSURLIsExcludedFromBackupKey error:nil];
     }
-
+    
+    // Set the new key
+    return [URL setResourceValue:[NSNumber numberWithBool:YES] forKey:NSURLIsExcludedFromBackupKey error:nil];
 }
 
 +(BOOL)networkAvailable{
