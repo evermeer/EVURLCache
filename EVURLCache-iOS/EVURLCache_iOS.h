73cb1a2a6fca2f13984ed28c74a8dfef388d9da7
Vitalii Kotivskyi <Vitalii_Kotivskyi@epam.com>
2019-04-02 15:40:04 +0300

    Added Carthage support

---
+++ EVURLCache-iOS/EVURLCache_iOS.h
@@ -0,0 +1,19 @@
+//
+//  EVURLCache_iOS.h
+//  EVURLCache-iOS
+//
+//  Created by Vitalii Kotivskyi on 4/2/19.
+//  Copyright © 2019 evict. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EVURLCache_iOS.
+FOUNDATION_EXPORT double EVURLCache_iOSVersionNumber;
+
+//! Project version string for EVURLCache_iOS.
+FOUNDATION_EXPORT const unsigned char EVURLCache_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EVURLCache_iOS/PublicHeader.h>
+
+
